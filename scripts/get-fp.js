20f1b12742ee282c22819a5d49c81e16bdf996ea
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-04-28 01:41:43 +0100

    Update get-fp.js

--- scripts/get-fp.js
+++ scripts/get-fp.js
@@ -4,7 +4,14 @@
 import puppeteer from 'puppeteer'
 
 (async () => {
-  var browser = await puppeteer.launch({ headless: true });
+  var browser = await puppeteer.launch({ 
+    headless: true,
+    args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage'
+  ]
+  });
   var page = await browser.newPage();
   await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36");
   await page.setViewport({ width: 1920, height: 1080 });
