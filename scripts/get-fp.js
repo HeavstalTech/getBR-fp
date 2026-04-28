38088bafa0c1a17ded555e731dbffbff451c9d7a
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-04-28 01:25:04 +0100

    Update get-fp.js

--- scripts/get-fp.js
+++ scripts/get-fp.js
@@ -1,10 +1,10 @@
 //script/get-fp.js
 // © Heavstal Tech 
 // umm...
-import puppeteer = from 'puppeteer'
+import puppeteer from 'puppeteer'
 
 (async () => {
-  var browser = await puppeteer.launch({ headless: "new" });
+  var browser = await puppeteer.launch({ headless: true });
   var page = await browser.newPage();
   await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36");
   await page.setViewport({ width: 1920, height: 1080 });
@@ -13,7 +13,6 @@ import puppeteer = from 'puppeteer'
     async function getFP() {
       let e = document.createElement("canvas"), t = e.getContext("2d");
       t.textBaseline = "top"; t.font = "14px Arial"; t.fillText("fp-canvas", 2, 2);
-      
       let s = [
         navigator.userAgent, 
         navigator.language, 
@@ -25,15 +24,13 @@ import puppeteer = from 'puppeteer'
         navigator.deviceMemory?.toString() ?? "", 
         (!!window.chrome).toString(), 
         e.toDataURL().slice(0, 200)
-      ].join("|");
-      
+      ].join("|")
       return Array.from(new Uint8Array(await crypto.subtle.digest("SHA-256", new TextEncoder().encode(s))))
         .map(e => e.toString(16).padStart(2, "0"))
-        .join("");
+        .join("")
     }
     return await getFP()
-  });
+  })
     console.log("browser fp:", FP)
-
   await browser.close()
-})();
+})()
