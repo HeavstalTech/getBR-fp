81c85459d4572aafd4814739b80a82099edb8721
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-04-28 01:50:14 +0100

    Update get-fp.js

--- scripts/get-fp.js
+++ scripts/get-fp.js
@@ -11,10 +11,11 @@ import puppeteer from 'puppeteer'
     '--disable-setuid-sandbox',
     '--disable-dev-shm-usage'
   ]
-  });
+  })
   var page = await browser.newPage();
-  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36");
-  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36")
+  await page.setViewport({ width: 1920, height: 1080 })
+  await page.goto("https://heavstal.com.ng")
 
   var FP = await page.evaluate(async () => {
     async function getFP() {
